efc0a4f9c77509bfb27b1dc8938b0e2f5f993cf1
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-05-16 20:05:10 +0200

    change files locations

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import Heading from "./Heading";
+import React from "react";
+
+function App() {
+  return <Heading />;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
 
-const date = new Date();
-const hour = date.getHours();
-let greeting = "";
-let inlineColor = "";
-
-if (hour < 12) {
-  greeting = "Good morning";
-  inlineColor = {
-    color: "red",
-  };
-} else if (hour < 18) {
-  greeting = "Good afternoon";
-  inlineColor = {
-    color: "green",
-  };
-} else if (hour > 18) {
-  greeting = "Good evening";
-  inlineColor = {
-    color: "blue",
-  };
-}
-
-ReactDOM.render(
-  <h1 style={inlineColor}>{greeting}</h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
